de6f49bac6bdfa96c7c726818b45b620857ad89e
Daniel <danielvalpereiro@gmail.com>
2025-03-12 22:38:53 +0000

    .

---
+++ Website/Cliente/src/app/(auth)/signin/[[...signin]]/page.tsx
@@ -0,0 +1,5 @@
+import SigninComponent from "@/components/SignIn"; // ✅ Nome começa com maiúscula
+
+export default function Page() {
+    return <SigninComponent />; // ✅ Usando corretamente o componente
+}
