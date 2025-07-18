82f4720de2508f555215811cf99e74d8554879ba
Daniel <danielvalpereiro@gmail.com>
2025-07-18 23:46:25 +0100

    .

--- Website/Cliente/src/app/not-found.tsx
+++ Website/Cliente/src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
         <h1 className="text-4xl font-bold mb-4">404</h1>
       <h1 className="text-4xl font-bold mb-4">Página não encontrada</h1>
       <p className="text-lg text-gray-600 mb-6">
-        A página que procuras não existe ou nã tem direito a acede-la.
+        A página que procuras não existe ou não tem direito a acede-la.
       </p>
       <a
         href="/"
